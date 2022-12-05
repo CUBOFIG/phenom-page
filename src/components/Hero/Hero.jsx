1d4915b223cbc36b41de11d63f943ce8e70d3f2e
CUBOFIG <hfigueroa@ucol.mx>
2022-12-04 23:27:08 -0600

    changes

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,17 +6,17 @@ import {
   TwitchChat,
 } from "./Hero.elements";
 import { useState } from "react";
-//import { changeState } from "../../features/live/liveSlices";
-//import { useDispatch } from "react-redux";
+import { changeState } from "../../features/live/liveSlices";
+import { useDispatch } from "react-redux";
 
 const Hero = () => {
   const [online, setOnline] = useState(true);
 
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // const changeCurrenState = (state) => {
-  //   dispatch(changeState(state));
-  // };
+  const changeCurrenState = (state) => {
+    dispatch(changeState(state));
+  };
 
   return (
     <TwitchContainer className="flex">
@@ -37,7 +37,7 @@ const Hero = () => {
             }}
             onOffline={() => {
               setOnline(false);
-              //changeCurrenState(false);
+              changeCurrenState(false);
             }}
           />
         </TwitchPlayerContainer>
